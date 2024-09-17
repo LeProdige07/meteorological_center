f46f416ffd9810aba0f26f89736486a53605c535
Don Makila <makiladon14@gmail.com>
2024-09-17 17:54:20 +0100

    fix

--- memoireDt/resources/views/monitoring/tableau.blade.php
+++ memoireDt/resources/views/monitoring/tableau.blade.php
@@ -44,20 +44,20 @@
                             <th>{{ $monitoring->humidite }} %</th>
                             <th>{{ $monitoring->pression_atm }} hPa</th>
                             <th>
-                                @if ($monitorings->luminosite < 3)
+                                @if ($monitoring->luminosite < 3)
                                     11+ : Extreme (Evitez toute exposition au soleil si possible.)
-                                @elseif ($monitorings->luminosite >= 3 && $monitorings->luminosite < 6)
+                                @elseif ($monitoring->luminosite >= 3 && $monitoring->luminosite < 6)
                                     10 : Très élevé (Limitez l'exposition au soleil)
-                                @elseif ($monitorings->luminosite >= 6 && $monitorings->luminosite < 8)
+                                @elseif ($monitoring->luminosite >= 6 && $monitoring->luminosite < 8)
                                     7 : Elevé (Evitez une exposition prolongée au soleil)
-                                @elseif ($monitorings->luminosite >= 8 && $monitorings->luminosite < 11)
+                                @elseif ($monitoring->luminosite >= 8 && $monitoring->luminosite < 11)
                                     5 : Modéré (Utilisez une protection contre le soleil)
                                 @else
                                     2 : Faible (Prendre des précautions si nécessaire)
                                 @endif
                             </th>
                             <th>
-                                @if ($monitorings->detection_pluie == 0)
+                                @if ($monitoring->detection_pluie == 0)
                                     Il pleut.
                                 @else
                                     Pas de pluie.
@@ -66,21 +66,21 @@
                             <th>{{ $monitoring->precipitation }} mm</th>
                             <th>{{ $monitoring->vitesse_vent }} Km/h</th>
                             <th>
-                                @if ($monitorings->direction_vent >= 2700 && $monitorings->direction_vent <= 2800)
+                                @if ($monitoring->direction_vent >= 2700 && $monitoring->direction_vent <= 2800)
                                     Nord
-                                @elseif ($monitorings->direction_vent >= 1600 && $monitorings->direction_vent <= 1700)
+                                @elseif ($monitoring->direction_vent >= 1600 && $monitoring->direction_vent <= 1700)
                                     Nord-Est
-                                @elseif ($monitorings->direction_vent >= 200 && $monitorings->direction_vent <= 300)
+                                @elseif ($monitoring->direction_vent >= 200 && $monitoring->direction_vent <= 300)
                                     Est
-                                @elseif ($monitorings->direction_vent >= 500 && $monitorings->direction_vent <= 600)
+                                @elseif ($monitoring->direction_vent >= 500 && $monitoring->direction_vent <= 600)
                                     Sud-Est
-                                @elseif ($monitorings->direction_vent >= 900 && $monitorings->direction_vent <= 1000)
+                                @elseif ($monitoring->direction_vent >= 900 && $monitoring->direction_vent <= 1000)
                                     Sud
-                                @elseif ($monitorings->direction_vent >= 2200 && $monitorings->direction_vent <= 2300)
+                                @elseif ($monitoring->direction_vent >= 2200 && $monitoring->direction_vent <= 2300)
                                     Sud-Ouest
-                                @elseif ($monitorings->direction_vent >= 3400 && $monitorings->direction_vent <= 3500)
+                                @elseif ($monitoring->direction_vent >= 3400 && $monitoring->direction_vent <= 3500)
                                     Ouest
-                                @elseif ($monitorings->direction_vent >= 3100 && $monitorings->direction_vent <= 3200)
+                                @elseif ($monitoring->direction_vent >= 3100 && $monitoring->direction_vent <= 3200)
                                     Nord-Est
                                 @endif
                             </th>
